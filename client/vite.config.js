1d71923203a84d75677346401ea9a252ceb66de8
seven272 <i727i@mail.ru>
2026-01-20 22:59:09 +0300

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app3/',
+  base: '/',
   server: {
     host: true,
     port: 5173, // This is the port which we will use in docker
     // allowedHosts: ['prank-sound.ru'],
   },
   preview: {
-    port: 6666,
+    port: 4444,
     strictPort: true,
   },
 })
