2821dab34daf79a806b8386b8ce76ba661ee5d02
seven272 <i727i@mail.ru>
2026-01-20 17:30:06 +0300

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'app3',
+  base: '/app3',
   server: {
     host: true,
     port: 5173, // This is the port which we will use in docker
